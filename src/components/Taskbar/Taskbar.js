ba5e34f02c70409c34044f1d0734febbb25eef78
Serhii Honchar <sergeygonchar88@gmail.com>
2021-12-02 17:28:36 +0200

    initial commit

---
+++ src/components/Taskbar/Taskbar.js
@@ -0,0 +1,11 @@
+import "./Taskbar.scss";
+
+const Taskbar = () => {
+    return (
+        <div className="taskbar">
+            Taskbar
+        </div>
+    )
+}
+
+export default Taskbar
---
+++ src/components/Window/Window.js
@@ -0,0 +1,11 @@
+import "./Window.scss";
+
+const Window = () => {
+    return (
+        <div className="windwo">
+            Window
+        </div>
+    )
+}
+
+export default Window
